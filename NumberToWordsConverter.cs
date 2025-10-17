72c697e543482df623dcc94840bb6163b7e0c5a9
renm201989-spec <renm201989@gmail.com>
2025-10-17 21:01:36 +0530

    upload

---
+++ NumberToWordsConverter.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvBillingSystem
+{
+    public static class NumberToWordsConverter
+    {
+        private static readonly string[] Ones =
+            { "Zero", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine",
+          "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen",
+          "Seventeen", "Eighteen", "Nineteen" };
+
+        private static readonly string[] Tens =
+            { "", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety" };
+
+        public static string ConvertAmountToWords(decimal amount)
+        {
+            long rupees = (long)Math.Floor(amount);
+            int paise = (int)((amount - rupees) * 100);
+
+            string words = ConvertNumberToWords(rupees) + " Rupees";
+
+            if (paise > 0)
+                words += " and " + ConvertNumberToWords(paise) + " Paise";
+
+            return words + " Only";
+        }
+
+        private static string ConvertNumberToWords(long number)
+        {
+            if (number == 0)
+                return "Zero";
+
+            if (number < 0)
+                return "Minus " + ConvertNumberToWords(Math.Abs(number));
+
+            string words = "";
+
+            if ((number / 10000000) > 0)
+            {
+                words += ConvertNumberToWords(number / 10000000) + " Crore ";
+                number %= 10000000;
+            }
+
+            if ((number / 100000) > 0)
+            {
+                words += ConvertNumberToWords(number / 100000) + " Lakh ";
+                number %= 100000;
+            }
+
+            if ((number / 1000) > 0)
+            {
+                words += ConvertNumberToWords(number / 1000) + " Thousand ";
+                number %= 1000;
+            }
+
+            if ((number / 100) > 0)
+            {
+                words += ConvertNumberToWords(number / 100) + " Hundred ";
+                number %= 100;
+            }
+
+            if (number > 0)
+            {
+                if (words != "")
+                    words += "and ";
+
+                if (number < 20)
+                    words += Ones[number];
+                else
+                {
+                    words += Tens[number / 10];
+                    if ((number % 10) > 0)
+                        words += " " + Ones[number % 10];
+                }
+            }
+
+            return words.Trim();
+        }
+    }
+
+}
