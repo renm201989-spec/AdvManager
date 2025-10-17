ccea9a89e3abb935ddf5b3d74dc96dd0b3d45959
renm201989-spec <renm201989@gmail.com>
2025-10-17 22:54:01 +0530

    Model

---
+++ Client.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvBillingSystem.ACM
+{
+    public class Client
+    {
+        public int ClientId { get; set; }
+        public string CaseNumber { get; set; }
+        public string CaseType { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Mobile { get; set; }
+        public string Email { get; set; }
+        public string Remarks { get; set; }
+        public string VisitedDt { get; set; }
+        public decimal? TotalAmount { get; set; }
+        public decimal? TotalFees { get; set; } = 0;
+        public decimal? OtherFees { get; set; } = 0;
+        public decimal? TotalPaid { get; set; } = 0;
+        public decimal? Balance { get; set; } = 0;
+        public int ActiveUser { get; set; } = 1;
+    }
+}
---
+++ Payment.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdvBillingSystem.ACM
+{
+    public class Payment
+    {
+        public int PaymentId { get; set; }
+        public int ClientId { get; set; }
+        public string CaseType { get; set; }
+        public DateTime PaymentDate { get; set; }
+        public decimal AmountPaid { get; set; }
+        public string Remarks { get; set; }
+        public decimal? CourtFees { get; set; } = 0;
+        public decimal? ClericalFees { get; set; } = 0;
+
+    }
+}
